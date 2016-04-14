7502628381f2b484b21e36a244078d7399810b47
zveruger <mkorolev@hill30.com>
2016-04-14 16:53:07 +0300

    Initial commit

---
+++ src/NSLookup/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using NSLookup.Properties;
+
+namespace NSLookup
+{
+    class Program
+    {
+        #region private
+        private static void _dnsLookup(string hostNameOrAddress)
+        {
+            try
+            {
+                var hostEntry = Dns.GetHostEntry(hostNameOrAddress);
+                _showMessage($"{Resources.MessageHostName}: {hostEntry.HostName}");
+
+                foreach (var ipAddress in hostEntry.AddressList)
+                {
+                    switch (ipAddress.AddressFamily)
+                    {
+                        case AddressFamily.InterNetwork:
+                            { _showMessage($"{Resources.MessageHostIPv4}: {ipAddress}"); }
+                            break;
+                        case AddressFamily.InterNetworkV6:
+                            { _showMessage($"{Resources.MessageHostIPv6}: {ipAddress}"); }
+                            break;
+                    }
+                }
+
+            }
+            catch (SocketException)
+            {
+                _showMessage(string.Format(Resources.MessageErrorNotDNSInformation, hostNameOrAddress));
+            }
+        }
+        //---------------------------------------------------------------------
+        private static void _clearMessages()
+        {
+            Console.Clear();
+        }
+        private static void _showMessage(string message, bool newLine = true)
+        {
+            if (newLine)
+            { Console.WriteLine(message); }
+            else
+            { Console.Write(message); }
+        }
+        #endregion
+
+        [STAThread]
+        static void Main()
+        {
+            do
+            {
+                _clearMessages();
+                _showMessage(Resources.MessageEnterIPAddress, false);
+
+                var hostNameOrAddress = Console.ReadLine();
+
+                _showMessage(string.Empty);
+                _dnsLookup(hostNameOrAddress);
+
+                _showMessage(string.Empty);
+                _showMessage(Resources.MessageExit);
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ src/NSLookup/Properties/AssemblyInfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+//-----------------------------------------------------------------------------
+[assembly: AssemblyTitle("NSLookup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NSLookup")]
+[assembly: AssemblyCopyright("Copyright © Max Korolev 2016. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+//-----------------------------------------------------------------------------
+[assembly: SuppressIldasm]
+//-----------------------------------------------------------------------------
+[assembly: ComVisible(false)]
+[assembly: Guid("206BFC8C-56E9-490F-853E-C7F84A5E89C0")]
+//-----------------------------------------------------------------------------
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
+//-----------------------------------------------------------------------------
